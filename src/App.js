f74a07a162abaf2200a2cdea5eed7afa53f10410
fevinfm <fevinfm2000@gmail.com>
2022-12-26 11:14:41 +0530

    task 26_12_2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        products: []
+    }
+}
+componentDidMount() {
+    fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json")
+    .then(res => res.json())
+    .then(
+        (products) => {
+            this.setState({ products: products });
+        },
+        (error) => {
+            alert(error);
+        }
+    )
+}
+  render() {
+    return (
+    <div>
+    <h1 className="heading">Products</h1>
+    <div className="box">
+    <table className="box" cellPadding="3px" cellSpacing="5px" >
+        <thead>
+            <tr>
+                <th className="grid-container">Product</th>
+                <th className="grid-container">Price</th>
+                <th className="grid-container">image</th>
+                <th className="grid-container">type</th>
+            </tr>
+        </thead>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <tbody>
+            {this.state.products.map(product =>
+                <tr>
+                    <td>{product.name}</td>
+                    <td>{product.price}</td>
+                    <td>{product.image}</td>
+                    <td>{product.type}</td>
+                </tr>
+            )}
+        </tbody>
+    </table>
     </div>
-  );
+    </div>);
+  }
 }
 
 export default App;
+
